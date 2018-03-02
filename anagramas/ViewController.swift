56f7939fddde7194407f42e0922a68357cca098a
ariel leventhal <arielleventhal@gmail.com>
2018-03-01 16:10:10 -0800

    Initial Commit

---
+++ anagramas/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  anagramas
+//
+//  Created by ariel leventhal on 01/03/18.
+//  Copyright © 2018 ariel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
