755b6bcfe58a1ae76a9a396ae446b5b01598ea4e
Unknown <arielleventhal@gmail.com>
2018-03-01 17:39:46 -0800

    working with textView

--- anagramas/ViewController.swift
+++ anagramas/ViewController.swift
@@ -10,18 +10,52 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var repetitionsTF: NSTextField!
+    @IBOutlet weak var nameTF: NSTextField!
+   
+    @IBOutlet var textView: NSTextView!
+    
+    
+    var anagrams = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func generate(_ sender: Any) {
+        anagrams.removeAll()
+        
+        var anagram = [Character]()
+        guard let text = nameTF.accessibilityValue()?.lowercased(), text.count > 0 else {return}
+        for _ in 1...Int(repetitionsTF.accessibilityValue()!)! {
+            for character in text {
+                let randomInt = Int(arc4random_uniform(UInt32(anagram.count + 1)))
+                anagram.insert(character, at: randomInt)
+            }
+            
+            if !anagrams.contains(String(anagram)) {
+                anagrams.append(String(anagram))
+            }
+            print(anagram)
+            anagram.removeAll()
+            
+        }
+        textView.textStorage?.append(NSAttributedString(string: anagrams.description))
+    }
+    
+ 
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
 }
+
+    
+    
+
+
 
